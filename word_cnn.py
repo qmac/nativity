39ca1c0de39419f7db7d53d9cdc567395bccc90e
qmac <quinten.mcnamara@gmail.com>
2017-05-11 00:55:46 -0500

    stylo tensor

--- word_cnn.py
+++ word_cnn.py
@@ -14,34 +14,36 @@ http://nlp.stanford.edu/data/glove.6B.zip
 http://www.cs.cmu.edu/afs/cs.cmu.edu/project/theo-20/www/data/news20.html
 '''
 
-from __future__ import print_function
-
 import numpy as np
 import sys
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.layers import Dense, Input, Flatten
 from keras.layers import Conv1D, MaxPooling1D, Embedding, Dropout, LSTM
+from keras.layers.merge import concatenate
 from keras.models import Model
 from keras.utils import to_categorical
 from gensim.models.keyedvectors import KeyedVectors
 from nltk.tokenize import sent_tokenize
 
-from run_model import load_files, encode_labels
+from run_model import load_files, encode_labels, load_features
 from features import POSTokenizer
 from evaluation import voting_test
 
 
-WORD_VEC_FILE = '../trained_vectors.bin'
+WORD_VEC_FILE = '../GoogleNews-vectors-negative300.bin'
+# WORD_VEC_FILE = '../trained_vectors.bin'
 TEXT_DATA_DIR = '../nli-shared-task-2017/data/essays/'
 MAX_SEQUENCE_LENGTH = 1000
 MAX_NB_WORDS = 72
+MAX_NB_WORDS = 20000
 EMBEDDING_DIM = 300
 VALIDATION_SPLIT = 0.2
 NUM_LABELS = 11
 PRETRAINED_EMBEDDINGS = False
 USE_POS_TAGS = False
 USE_DROPOUT = False
+SAVE_MODEL = False
 
 
 def expand_labels(labels, num_sentence_dict):
@@ -75,11 +77,11 @@ def create_character_tensor(files, tokenizer=None):
             texts.append(a)
 
     b = pad_sequences(texts, maxlen=MAX_SEQUENCE_LENGTH)
-    
-
-    # data = [ np.eye(71)[b[i]] for i in range(len(b))]        
-
     return np.asarray(b)
+
+
+create_stylo_tensor = load_features
+
 
 def create_sentence_tensor(files, tokenizer=None):
     texts = []  # list of text samples
@@ -163,6 +165,8 @@ if __name__ == '__main__':
     data = create_character_tensor(train_files)
     word_index = np.arange(71)
 
+    # stylo_tensor = create_stylo_tensor(train_files)
+
     # labels = expand_labels(labels, num_sentence_dict)
 
     print('Shape of data tensor:', data.shape)
@@ -173,12 +177,15 @@ if __name__ == '__main__':
     np.random.shuffle(indices)
     data = data[indices]
     labels = labels[indices]
+    stylo_tensor = stylo_tensor[indices]
     num_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
 
     x_train = data[:-num_validation_samples]
     y_train = labels[:-num_validation_samples]
     x_val = data[-num_validation_samples:]
     y_val = labels[-num_validation_samples:]
+    stylo_train = stylo_tensor[:-num_validation_samples]
+    stylo_val = stylo_tensor[-num_validation_samples:]
 
     num_words = min(MAX_NB_WORDS, len(word_index)+1)
 
@@ -206,6 +213,7 @@ if __name__ == '__main__':
                                     weights=[embedding_matrix],
                                     input_length=MAX_SEQUENCE_LENGTH,
                                     trainable=False)
+        wvModel = None
     else:
         print('Preparing embedding layer')
 
@@ -224,26 +232,38 @@ if __name__ == '__main__':
     x = Flatten()(x)
     x = Dense(128, activation='relu')(x)
     x = Dropout(0.2)(x) if USE_DROPOUT else x
+
+    #stylo = Input(shape=(7,))
+    #x = concatenate([x, stylo])
     preds = Dense(NUM_LABELS, activation='softmax')(x)
 
+    #model = Model([sequence_input, stylo], preds)
     model = Model(sequence_input, preds)
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam',
                   metrics=['acc'])
     print(model.summary())
+
+    #model.fit([x_train, stylo_train], y_train,
     model.fit(x_train, y_train,
               batch_size=128,
-              epochs=15,
+              epochs=10,
               validation_data=(x_val, y_val))
-
+              #validation_data=([x_val, stylo_val], y_val))
 
     # Evaluate
     # x_test, _, _, test_sentence_dict = create_sentence_tensor(test_files, tokenizer=tokenizer)
 
     # y_test = expand_labels(to_categorical(np.array(encode_labels(test_labels))), test_sentence_dict)
     x_test = create_character_tensor(test_files)
-    y_test = np.eye(11)[encode_labels(test_labels)]
+    y_test = to_categorical(np.array(encode_labels(test_labels)))
     print(model.evaluate(x_test, y_test))
 
-    model.save('char_cnn_model.hdf')
+    if SAVE_MODEL:
+        model.save('char_cnn_model.hdf')
+        
+    #x_test, _, _ = create_tensor(test_files, tokenizer=tokenizer)
+    #y_test = to_categorical(np.array(encode_labels(test_labels)))
+    #stylo_test = create_stylo_tensor(test_files)
+    #print(model.evaluate([x_test, stylo_test], y_test))
     # voting_test(model, x_test, test_labels, test_sentence_dict)
