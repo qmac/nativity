5f2d9155e08453907ea86c84aaf44d62e05cdc01
qmac <quinten.mcnamara@gmail.com>
2017-05-11 01:26:49 -0500

    refactored tensors

---
+++ tensors.py
@@ -0,0 +1,101 @@
+import numpy as np
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from nltk.tokenize import sent_tokenize
+
+from features import POSTokenizer
+from run_model import load_features
+
+
+def expand_labels(labels, num_sentence_dict):
+    expanded_labels = []
+    for i in range(len(labels)):
+        num_sentences = num_sentence_dict[i]
+        expanded_labels.extend(num_sentences * [labels[i]])
+
+    return np.array(expanded_labels)
+
+
+def char_to_i(c):
+    c = c.upper()
+    n = ord(c)
+    if n < 32:
+        if n == 9 or n == 10:
+            return n + 60
+    if n > 96:
+        return n - 32 - 26
+    else:
+        return n - 32
+
+
+def create_character_tensor(files, tokenizer=None, max_seq_length=1000):
+    texts = []
+
+    for i, fname in enumerate(files):
+        with open(fname) as f:
+            text = f.read()
+            a = [char_to_i(c) for c in list((text.upper()))]
+            texts.append(a)
+
+    b = pad_sequences(texts, maxlen=max_seq_length)
+    return np.asarray(b)
+
+
+create_stylo_tensor = load_features
+
+
+def create_sentence_tensor(files, tokenizer=None, use_pos=False, max_seq_length=50, max_words=20000):
+    texts = []  # list of text samples
+    num_sentence_dict = {}
+    for i, fname in enumerate(files):
+        with open(fname) as f:
+            text = f.read()
+            sentences = sent_tokenize(text)
+            num_sentence_dict[i] = len(sentences)
+            for s in sentences:
+                if use_pos:
+                    t = POSTokenizer()
+                    texts.append(' '.join(t(s)))
+                else:
+                    texts.append(s)
+
+    print('Found %s texts.' % len(texts))
+
+    # finally, vectorize the text samples into a 2D integer tensor
+    if tokenizer is None:
+        tokenizer = Tokenizer(num_words=max_words)
+        tokenizer.fit_on_texts(texts)
+    sequences = tokenizer.texts_to_sequences(texts)
+
+    word_index = tokenizer.word_index
+    print('Found %s unique tokens.' % len(word_index))
+
+    data = pad_sequences(sequences, maxlen=max_seq_length)
+
+    return data, word_index, tokenizer, num_sentence_dict
+
+
+def create_tensor(files, tokenizer=None, use_pos=False, max_seq_length=1000, max_words=20000):
+    texts = []  # list of text samples
+    for fname in files:
+        with open(fname) as f:
+            if use_pos:
+                t = POSTokenizer()
+                texts.append(' '.join(t(f.read())))
+            else:
+                texts.append(f.read())
+
+    print('Found %s texts.' % len(texts))
+
+    # finally, vectorize the text samples into a 2D integer tensor
+    if tokenizer is None:
+        tokenizer = Tokenizer(num_words=max_words)
+        tokenizer.fit_on_texts(texts)
+    sequences = tokenizer.texts_to_sequences(texts)
+
+    word_index = tokenizer.word_index
+    print('Found %s unique tokens.' % len(word_index))
+
+    data = pad_sequences(sequences, maxlen=max_seq_length)
+
+    return data, word_index, tokenizer
--- word_cnn.py
+++ word_cnn.py
@@ -15,20 +15,19 @@ http://www.cs.cmu.edu/afs/cs.cmu.edu/project/theo-20/www/data/news20.html
 '''
 
 import numpy as np
-import sys
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
 from keras.layers import Dense, Input, Flatten
 from keras.layers import Conv1D, MaxPooling1D, Embedding, Dropout, LSTM
 from keras.layers.merge import concatenate
 from keras.models import Model
 from keras.utils import to_categorical
 from gensim.models.keyedvectors import KeyedVectors
-from nltk.tokenize import sent_tokenize
 
-from run_model import load_files, encode_labels, load_features
-from features import POSTokenizer
+from run_model import load_files, encode_labels
 from evaluation import voting_test
+from tensors import (create_character_tensor,
+                     create_sentence_tensor,
+                     create_tensor,
+                     create_stylo_tensor)
 
 
 WORD_VEC_FILE = '../GoogleNews-vectors-negative300.bin'
@@ -36,7 +35,6 @@ WORD_VEC_FILE = '../GoogleNews-vectors-negative300.bin'
 TEXT_DATA_DIR = '../nli-shared-task-2017/data/essays/'
 MAX_SEQUENCE_LENGTH = 1000
 MAX_NB_WORDS = 72
-MAX_NB_WORDS = 20000
 EMBEDDING_DIM = 300
 VALIDATION_SPLIT = 0.2
 NUM_LABELS = 11
@@ -44,100 +42,6 @@ PRETRAINED_EMBEDDINGS = False
 USE_POS_TAGS = False
 USE_DROPOUT = False
 SAVE_MODEL = False
-
-
-def expand_labels(labels, num_sentence_dict):
-    expanded_labels = []
-    for i in range(len(labels)):
-        num_sentences = num_sentence_dict[i]
-        expanded_labels.extend(num_sentences * [labels[i]])
-
-    return np.array(expanded_labels)
-
-
-def char_to_i(c):
-    c = c.upper()
-    n = ord(c)
-    if n < 32:
-        if n == 9 or n == 10:
-            return n + 60
-    if n > 96:
-        return n - 32 - 26
-    else:
-        return n - 32
-
-
-def create_character_tensor(files, tokenizer=None):
-    texts = []
-
-    for i, fname in enumerate(files):
-        with open(fname) as f:
-            text = f.read()
-            a = [char_to_i(c) for c in list((text.upper()))]
-            texts.append(a)
-
-    b = pad_sequences(texts, maxlen=MAX_SEQUENCE_LENGTH)
-    return np.asarray(b)
-
-
-create_stylo_tensor = load_features
-
-
-def create_sentence_tensor(files, tokenizer=None):
-    texts = []  # list of text samples
-    num_sentence_dict = {}
-    for i, fname in enumerate(files):
-        with open(fname) as f:
-            text = f.read()
-            sentences = sent_tokenize(text)
-            num_sentence_dict[i] = len(sentences)
-            for s in sentences:
-                if USE_POS_TAGS:
-                    t = POSTokenizer()
-                    texts.append(' '.join(t(s)))
-                else:
-                    texts.append(s)
-
-    print('Found %s texts.' % len(texts))
-
-    # finally, vectorize the text samples into a 2D integer tensor
-    if tokenizer is None:
-        tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
-        tokenizer.fit_on_texts(texts)
-    sequences = tokenizer.texts_to_sequences(texts)
-
-    word_index = tokenizer.word_index
-    print('Found %s unique tokens.' % len(word_index))
-
-    data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
-
-    return data, word_index, tokenizer, num_sentence_dict
-
-
-def create_tensor(files, tokenizer=None):
-    texts = []  # list of text samples
-    for fname in files:
-        with open(fname) as f:
-            if USE_POS_TAGS:
-                t = POSTokenizer()
-                texts.append(' '.join(t(f.read())))
-            else:
-                texts.append(f.read())
-
-    print('Found %s texts.' % len(texts))
-
-    # finally, vectorize the text samples into a 2D integer tensor
-    if tokenizer is None:
-        tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
-        tokenizer.fit_on_texts(texts)
-    sequences = tokenizer.texts_to_sequences(texts)
-
-    word_index = tokenizer.word_index
-    print('Found %s unique tokens.' % len(word_index))
-
-    data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
-
-    return data, word_index, tokenizer
 
 
 if __name__ == '__main__':
@@ -162,7 +66,7 @@ if __name__ == '__main__':
 
     # Create data tensor
     # data, word_index, tokenizer, num_sentence_dict = create_sentence_tensor(train_files)
-    data = create_character_tensor(train_files)
+    data = create_character_tensor(train_files, max_seq_length=MAX_SEQUENCE_LENGTH)
     word_index = np.arange(71)
 
     # stylo_tensor = create_stylo_tensor(train_files)
@@ -177,15 +81,15 @@ if __name__ == '__main__':
     np.random.shuffle(indices)
     data = data[indices]
     labels = labels[indices]
-    stylo_tensor = stylo_tensor[indices]
+    # stylo_tensor = stylo_tensor[indices]
     num_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
 
     x_train = data[:-num_validation_samples]
     y_train = labels[:-num_validation_samples]
     x_val = data[-num_validation_samples:]
     y_val = labels[-num_validation_samples:]
-    stylo_train = stylo_tensor[:-num_validation_samples]
-    stylo_val = stylo_tensor[-num_validation_samples:]
+    # stylo_train = stylo_tensor[:-num_validation_samples]
+    # stylo_val = stylo_tensor[-num_validation_samples:]
 
     num_words = min(MAX_NB_WORDS, len(word_index)+1)
 
@@ -233,35 +137,35 @@ if __name__ == '__main__':
     x = Dense(128, activation='relu')(x)
     x = Dropout(0.2)(x) if USE_DROPOUT else x
 
-    #stylo = Input(shape=(7,))
-    #x = concatenate([x, stylo])
+    # stylo = Input(shape=(7,))
+    # x = concatenate([x, stylo])
     preds = Dense(NUM_LABELS, activation='softmax')(x)
 
-    #model = Model([sequence_input, stylo], preds)
+    # model = Model([sequence_input, stylo], preds)
     model = Model(sequence_input, preds)
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam',
                   metrics=['acc'])
     print(model.summary())
 
-    #model.fit([x_train, stylo_train], y_train,
+    # model.fit([x_train, stylo_train], y_train,
     model.fit(x_train, y_train,
               batch_size=128,
               epochs=10,
               validation_data=(x_val, y_val))
-              #validation_data=([x_val, stylo_val], y_val))
+              # validation_data=([x_val, stylo_val], y_val))
 
     # Evaluate
     # x_test, _, _, test_sentence_dict = create_sentence_tensor(test_files, tokenizer=tokenizer)
 
     # y_test = expand_labels(to_categorical(np.array(encode_labels(test_labels))), test_sentence_dict)
-    x_test = create_character_tensor(test_files)
+    x_test = create_character_tensor(test_files, max_seq_length=MAX_SEQUENCE_LENGTH)
     y_test = to_categorical(np.array(encode_labels(test_labels)))
     print(model.evaluate(x_test, y_test))
 
     if SAVE_MODEL:
         model.save('char_cnn_model.hdf')
-        
+
     #x_test, _, _ = create_tensor(test_files, tokenizer=tokenizer)
     #y_test = to_categorical(np.array(encode_labels(test_labels)))
     #stylo_test = create_stylo_tensor(test_files)
